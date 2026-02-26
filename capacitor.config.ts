9ec1f039910021bbe4a542c9575a08885ab460c1
MatheusGL <31011047+MatheusGL@users.noreply.github.com>
2026-02-26 13:31:05 -0300

    Adiciona Capacitor iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ibbra.bpo',
+  appName: 'BPO Ibbra',
+  webDir: 'dist'
+};
+
+export default config;
